66461ff35c31e56dceb5843314390a4c861c295e
Svetozar Nesic <snesic@gmail.com>
2025-08-11 18:35:39 +0200

    remove plots from postprocessing

--- R/align.R
+++ R/align.R
@@ -30,69 +30,131 @@
 #' @return dat A dataframe containing information on the resulting alignments
 #' output <- align(regimen,drugRec)
 #' @export
-align <- function(regimen,regName,drugRec,g,Tfac,s=NA,verbose,mem,removeOverlap,method) {
-
-  if(!exists("temporal_alignment", mode="function")) {
-    reticulate::source_python(system.file("python/init.py",package="ARTEMIS"),envir=globalenv())
-    reticulate::source_python(system.file("python/score.py",package="ARTEMIS"),envir=globalenv())
-    reticulate::source_python(system.file("python/align.py",package="ARTEMIS"),envir=globalenv())
-    reticulate::source_python(system.file("python/main.py",package="ARTEMIS"),envir=globalenv())
-  }
-
-  if(typeof(regimen[[1]]) == "list"){
-    if(typeof(regName) == "character") {
-      print("Multiple regimens but only one regname. Please check regnames.")
-      return(NA)
+align <- function(regimen,
+                  regName,
+                  drugRec,
+                  g = 0.4,
+                  Tfac = 0.5,
+                  s = NA,
+                  verbose = 0,
+                  mem = -1,
+                  removeOverlap = 1,
+                  method = "PropDiff") {
+    if (!exists("temporal_alignment", mode = "function")) {
+        reticulate::source_python(system.file("python/init.py", package = "ARTEMIS"), envir = globalenv())
+        reticulate::source_python(system.file("python/score.py", package = "ARTEMIS"), envir = globalenv())
+        reticulate::source_python(system.file("python/align.py", package = "ARTEMIS"), envir = globalenv())
+        reticulate::source_python(system.file("python/main.py", package = "ARTEMIS"), envir = globalenv())
     }
-
-    if(is.na(s)){
-      s <- defaultSmatrix(unlist(regimen, recursive = F),drugRec)
+    
+    if (typeof(regimen[[1]]) == "list") {
+        if (typeof(regName) == "character") {
+            print("Multiple regimens but only one regname. Please check regnames.")
+            return(NA)
+        }
+        
+        if (is.na(s)) {
+            s <- defaultSmatrix(unlist(regimen, recursive = F), drugRec)
+        }
+        
+        dat <- as.data.frame(matrix(nrow = 0, ncol = 10))
+        colnames(dat) <- c(
+            "regName",
+            "Regimen",
+            "DrugRecord",
+            "Score",
+            "regimen_Start",
+            "regimen_End",
+            "drugRec_Start",
+            "drugRec_End",
+            "Aligned_Seq_len",
+            "totAlign"
+        )
+        
+        for (i in c(1:length(regimen))) {
+            temp_dat <- temporal_alignment(
+                regimen[[i]],
+                regName[[i]],
+                drugRec,
+                g,
+                Tfac,
+                as.data.frame(s),
+                verbose,
+                mem,
+                removeOverlap,
+                method
+            )
+            temp_dat <- as.data.frame(temp_dat)
+            
+            colnames(temp_dat) <- c(
+                "regName",
+                "Regimen",
+                "DrugRecord",
+                "Score",
+                "regimen_Start",
+                "regimen_End",
+                "drugRec_Start",
+                "drugRec_End",
+                "Aligned_Seq_len",
+                "totAlign"
+            )
+            
+            temp_dat[1, ]$Regimen <- decode(regimen[[i]])
+            temp_dat[1, ]$DrugRecord <- decode(drugRec)
+            temp_dat$Regimen <- gsub("^;", "", temp_dat$Regimen)
+            temp_dat$DrugRecord <- gsub("^;", "", temp_dat$DrugRecord)
+            
+            temp_dat$adjustedS <- as.numeric(temp_dat$Score) / as.numeric(temp_dat$totAlign)
+            
+            dat <- rbind(dat, temp_dat)
+            
+        }
+        
+        return(dat)
+        
+    } else if (typeof(regimen[[1]]) == "character") {
+        if (is.na(s)) {
+            s <- defaultSmatrix(regimen, drugRec)
+        }
+        
+        dat <- temporal_alignment(
+            regimen,
+            regName,
+            drugRec,
+            g,
+            Tfac,
+            as.data.frame(s),
+            verbose,
+            mem,
+            removeOverlap,
+            method
+        )
+        dat <- as.data.frame(dat)
+        
+        colnames(dat) <- c(
+            "regName",
+            "Regimen",
+            "DrugRecord",
+            "Score",
+            "regimen_Start",
+            "regimen_End",
+            "drugRec_Start",
+            "drugRec_End",
+            "Aligned_Seq_len",
+            "totAlign"
+        )
+        
+        dat[1, ]$Regimen <- decode(regimen)
+        dat[1, ]$DrugRecord <- decode(drugRec)
+        dat$Regimen <- gsub("^;", "", dat$Regimen)
+        dat$DrugRecord <- gsub("^;", "", dat$DrugRecord)
+        
+        dat$adjustedS <- as.numeric(dat$Score) / as.numeric(dat$totAlign)
+        
+        dat <- dat %>%
+            filter(totAlign != 0, totAlign != -1, (adjustedS > 0 |
+                                                       is.na(adjustedS)))
+        
+        return(dat)
     }
-
-    dat <-as.data.frame(matrix(nrow=0,ncol=10))
-    colnames(dat) <- c("regName","Regimen","DrugRecord","Score","regimen_Start","regimen_End","drugRec_Start","drugRec_End","Aligned_Seq_len","totAlign")
-
-    for(i in c(1:length(regimen))) {
-
-      temp_dat <- temporal_alignment(regimen[[i]],regName[[i]],drugRec,g,Tfac,as.data.frame(s), verbose, mem, removeOverlap, method)
-      temp_dat <- as.data.frame(temp_dat)
-
-      colnames(temp_dat) <- c("regName","Regimen","DrugRecord","Score","regimen_Start","regimen_End","drugRec_Start","drugRec_End","Aligned_Seq_len","totAlign")
-
-      temp_dat[1,]$Regimen <- decode(regimen[[i]])
-      temp_dat[1,]$DrugRecord <- decode(drugRec)
-      temp_dat$Regimen <- gsub("^;","",temp_dat$Regimen)
-      temp_dat$DrugRecord <- gsub("^;","",temp_dat$DrugRecord)
-
-      temp_dat$adjustedS <- as.numeric(temp_dat$Score)/as.numeric(temp_dat$totAlign)
-
-      dat <- rbind(dat,temp_dat)
-
-    }
-
-    return(dat)
-
-  } else if(typeof(regimen[[1]]) == "character") {
-
-    if(is.na(s)){
-      s <- defaultSmatrix(regimen,drugRec)
-    }
-
-    dat <- temporal_alignment(regimen,regName,drugRec,g,Tfac,as.data.frame(s), verbose, mem, removeOverlap, method)
-    dat <- as.data.frame(dat)
-
-    colnames(dat) <- c("regName","Regimen","DrugRecord","Score","regimen_Start","regimen_End","drugRec_Start","drugRec_End","Aligned_Seq_len","totAlign")
-
-    dat[1,]$Regimen <- decode(regimen)
-    dat[1,]$DrugRecord <- decode(drugRec)
-    dat$Regimen <- gsub("^;","",dat$Regimen)
-    dat$DrugRecord <- gsub("^;","",dat$DrugRecord)
-
-    dat$adjustedS <- as.numeric(dat$Score)/as.numeric(dat$totAlign)
-
-    dat <- dat %>%
-      filter(totAlign != 0, totAlign != -1, (adjustedS > 0|is.na(adjustedS)))
-
-    return(dat)
-  }
 }
