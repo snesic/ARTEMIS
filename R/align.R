2feed39becdb434c617a747a9b1d955650818eab
Svetozar Nesic <snesic@gmail.com>
2025-09-03 17:39:07 +0200

    remove list regimens from alignment

--- R/align.R
+++ R/align.R
@@ -4,8 +4,8 @@
 #'
 #' @param regimen A sequence to be aligned, usually a short sequence defining a chemotherapy regimen,
 #'                may contain multiple regimens if supplied as a list
-#' @param regName The name of the supplied regimen/s, may be a character or a list
-#' @param drugRec A longer sequence to be aligned, usually a patient's converted drug occurrence table
+#' @param regName The name of the supplied regimen
+#' @param drugRec A patient's converted drug occurrence table
 #' @param g A gap penalty supplied to the temporal needleman wunsch/smith waterman algorithms
 #' @param Tfac The time penalty factor. All time penalties are calculated as a percentage of Tfac
 #' @param s A substituion matrix, either user-defined or derived from defaultSmatrix. Will be auto-generated if left blank.
@@ -57,89 +57,44 @@ align <- function(regimen,
               totAlign       = numeric()
             )
     
-    if (typeof(regimen[[1]]) == "list") {
-        if (typeof(regName) == "character") {
-            print("Multiple regimens but only one regname. Please check regnames.")
-            return(NA)
-        }
-        
-        if (is.na(s)) {
-            s <- defaultSmatrix(unlist(regimen, recursive = F), drugRec)
-        }
-        
-        
-        for (i in c(1:length(regimen))) {
-            temp_dat <- temporal_alignment(
-                regimen[[i]],
-                regName[[i]],
-                drugRec,
-                g,
-                Tfac,
-                as.data.frame(s),
-                verbose,
-                mem,
-                removeOverlap,
-                method
-            )
-            temp_dat <- as.data.frame(temp_dat)
-            names(temp_dat) <- names(dat)
-            temp_dat <- temp_dat %>%
-                dplyr::mutate(dplyr::across(c(Score, regimen_Start, regimen_End, 
-                                            drugRec_Start, drugRec_End,
-                                            Aligned_Seq_len, totAlign), 
-                              as.numeric))
-
-            temp_dat[1, ]$Regimen <- decode(regimen[[i]])
-            temp_dat[1, ]$DrugRecord <- decode(drugRec)
-            temp_dat$Regimen <- gsub("^;", "", temp_dat$Regimen)
-            temp_dat$DrugRecord <- gsub("^;", "", temp_dat$DrugRecord)
-            
-            temp_dat$adjustedS <- temp_dat$Score / temp_dat$totAlign
-            
-            dat <- rbind(dat, temp_dat)
-   
-        }
-        return(dat)
-        
-    } else if (typeof(regimen[[1]]) == "character") {
-        if (is.na(s)) {
-            s <- defaultSmatrix(regimen, drugRec)
-        }
-        
-        temp_dat <- temporal_alignment(
-            regimen,
-            regName,
-            drugRec,
-            g,
-            Tfac,
-            as.data.frame(s),
-            verbose,
-            mem,
-            removeOverlap,
-            method
-        )
-        temp_dat <- as.data.frame(temp_dat)
-        
-        names(temp_dat) <- names(dat)
-        
-        temp_dat <- temp_dat %>%
-                dplyr::mutate(dplyr::across(c(Score, regimen_Start, regimen_End, 
-                                              drugRec_Start, drugRec_End,
-                                              Aligned_Seq_len, totAlign), 
-                              as.numeric))
-        
-        temp_dat[1, ]$Regimen <- decode(regimen)
-        temp_dat[1, ]$DrugRecord <- decode(drugRec)
-        temp_dat$Regimen <- gsub("^;", "", temp_dat$Regimen)
-        temp_dat$DrugRecord <- gsub("^;", "", temp_dat$DrugRecord)
-        
-        temp_dat$adjustedS <- temp_dat$Score / temp_dat$totAlign
-        
-        temp_dat <- temp_dat %>%
-            dplyr::filter(totAlign != 0 | is.na(totAlign), 
-                          totAlign != -1 | is.na(totAlign), 
-                          (adjustedS > 0 | is.na(adjustedS)))
-
-        return(temp_dat)
+    
+    if (is.na(s)) {
+        s <- defaultSmatrix(regimen, drugRec)
     }
+        
+    temp_dat <- temporal_alignment(
+        regimen,
+        regName,
+        drugRec,
+        g,
+        Tfac,
+        as.data.frame(s),
+        verbose,
+        mem,
+        removeOverlap,
+        method
+    )
+    temp_dat <- as.data.frame(temp_dat)
+    
+    names(temp_dat) <- names(dat)
+    
+    temp_dat <- temp_dat %>%
+        dplyr::mutate(dplyr::across(c(Score, regimen_Start, regimen_End, 
+                                      drugRec_Start, drugRec_End,
+                                      Aligned_Seq_len, totAlign), 
+                      as.numeric))
+    
+    temp_dat[1, ]$Regimen <- decode(regimen)
+    temp_dat[1, ]$DrugRecord <- decode(drugRec)
+    temp_dat$Regimen <- gsub("^;", "", temp_dat$Regimen)
+    temp_dat$DrugRecord <- gsub("^;", "", temp_dat$DrugRecord)
+    
+    temp_dat$adjustedS <- temp_dat$Score / temp_dat$totAlign
+    
+    temp_dat <- temp_dat %>%
+        dplyr::filter(totAlign != 0 | is.na(totAlign), 
+                        totAlign != -1 | is.na(totAlign), 
+                        (adjustedS > 0 | is.na(adjustedS)))
+
+    return(temp_dat)
 }
